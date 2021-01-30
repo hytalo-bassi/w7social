f1bf183c53e49cc4db3de7e6bcaed68fdcbcb596
Hytalo M. Bassi <hytalombassi@gmail.com>
2021-01-30 10:47:23 +0000

    project structure

---
+++ web/dist/tailwind.config.dev.js
@@ -0,0 +1,17 @@
+"use strict";
+
+module.exports = {
+  purge: [],
+  darkMode: false,
+  // or 'media' or 'class'
+  theme: {
+    extend: {},
+    container: {
+      center: true
+    }
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+};
